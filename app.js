a9a5d4dd5f092c19fd5cd112dbddbe4f1b022207
abahad7921 <53481272+abahad7921@users.noreply.github.com>
2021-06-01 09:43:00 +0530

    new files added

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const btn = document.querySelector(".demo-btn");
+//console.log("this is working");
 
-btn.addEventListener("click", ()=>{
-    console.log("arrow function btn clicked")
+const inp = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const op = document.querySelector(".output-para");
+
+btn.addEventListener("click",()=>{
+    console.log(inp.value);
+    
+    const temp = inp.value;
+    
+    if(temp == "abdul ahad" || temp == "Abdul Ahad")
+    {
+        op.innerHTML = "yo! you know my correct name man";
+    }
+    else{
+        op.innerHTML = "nah!!!  I was wrong about you man";
+    }
+
 })
